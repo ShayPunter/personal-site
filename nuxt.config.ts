900ab393ae7ff6b43599e2ceb5ccca0ad3884b94
Shay Punter <icodinqs@gmail.com>
2022-05-19 21:42:51 +0100

    maybe it works now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,13 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	buildModules: ['@nuxtjs/sanity'],
+	modules: ['@nuxtjs/sanity'],
 	plugins: ['@/plugins/sanity-blocks.js'],
+
+	sanity: {
+		projectId: 'ul94kzl8',
+	},
+
 	vite: {
 		build: {
 			rollupOptions: {
