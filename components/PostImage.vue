6e9d192610857c901b7bcd7dba4f1cfdeb686a80
Shay Punter <icodinqs@gmail.com>
2022-05-29 00:03:43 +0100

    add postimage file

---
+++ components/PostImage.vue
@@ -0,0 +1,11 @@
+<template>
+	<pre>{{ JSON.stringify(_sanityProps, null, 2) }}</pre>
+	<!-- <SanityImage :asset-id="image" auto="format" loading="lazy" /> -->
+</template>
+
+<script>
+	export default {
+		props: ['_sanityProps'],
+		name: 'PostImage',
+	};
+</script>
