4686cbafdd6875975741b9f9e35af10784ae9351
Shay Punter <icodinqs@gmail.com>
2022-05-29 02:50:51 +0100

    Added head meta to portfolio page

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,5 +1,10 @@
 <template>
 	<NuxtLayout name="main">
+		<Head>
+			<Title>Portfolio</Title>
+			<Meta name="description" content="The portfolio of Shay Punter" />
+			<Meta rel="preconnect" href="https://cdn.sanity.io" />
+		</Head>
 		<!-- PORTFOLIO -->
 		<div
 			class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8"
